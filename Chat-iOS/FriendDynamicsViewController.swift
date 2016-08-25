3d6fd66b2f8599b92a4fd7b3ba509d58e68bbf3c
这块显卡有点冷 <root@dowhile.net>
2016-08-25 21:39:47 +0800

    JSONExport test success
    JSON Export 支持 Swift 3.0 使用原生的解析JSON 的办法

--- Chat-iOS/FriendDynamicsViewController.swift
+++ Chat-iOS/FriendDynamicsViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class FriendDynamicsViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -32,4 +33,18 @@ class FriendDynamicsViewController: UIViewController {
     }
     */
 
+}
+
+extension FriendDynamicsViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MomentsCell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
 }
